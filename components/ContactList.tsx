eb98330a9c2257f265d0f82d9408b950b5ce97f2
parteek kumar <digitalparteek@gmail.com>
2023-07-20 16:48:52 +0530

    Improved ContactList Styling

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -30,13 +30,15 @@ export default function ContactList() {
   ];
   return (
     <View>
-      <Text style={styles.headingText}>ContactList</Text>
+      <Text style={styles.headingText}>Contact List</Text>
       <ScrollView style={styles.container} scrollEnabled={false}>
         {contacts.map(({uid, name, status, imageUrl}) => (
           <View key={uid} style={styles.userCard}>
             <Image source={{uri: imageUrl}} style={styles.userImage} />
-            <Text style={styles.userName}>{name}</Text>
-            <Text style={styles.userStatus}>{status}</Text>
+            <View>
+              <Text style={styles.userName}>{name}</Text>
+              <Text style={styles.userStatus}>{status}</Text>
+            </View>
           </View>
         ))}
       </ScrollView>
@@ -45,14 +47,37 @@ export default function ContactList() {
 }
 
 const styles = StyleSheet.create({
-  headingText: {},
-  container: {},
-  userCard: {},
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  container: {
+    paddingHorizontal: 16,
+    marginBottom: 4,
+  },
+  userCard: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 3,
+    backgroundColor: '#8D3DAF',
+    padding: 8,
+    borderRadius: 10,
+  },
   userImage: {
     width: 60,
     height: 60,
     borderRadius: 60 / 2,
+    marginRight: 14,
   },
-  userName: {},
-  userStatus: {},
+  userName: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#FFFFFF',
+  },
+  userStatus: {
+    fontSize: 12,
+    color: '#FFFFFF',
+  },
 });
